18e8532b344f524c5e1b529c9180bba4a04459a0
Priyanka <priyankahansi95@gmail.com>
2022-06-15 23:41:56 +0530

    added:Maximum,minimum and sum of employee

---
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://127.0.0.1:3306/payroll?useSSL=false";
+        String username = "root";
+        String password = "123456789";
+        Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("cannot find the driver in the classpath!", e);
+        }
+        try {
+            System.out.println("connecting to database:" + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("connection done successful!!" + con);
+            Statement statement=con.createStatement();
+//            statement.execute();
+            ResultSet resultSet =statement.executeQuery("select Gender,sum(salary) as sum," +
+                    "avg(salary) \n" +
+                    "as average,max(salary) as maximum,min(salary) as minimum\n" +
+                    "from employee group by Gender");
+            while(resultSet.next()){
+                System.out.println("id:"+resultSet.getInt("id"));
+                System.out.println("name:"+resultSet.getString("name"));
+                System.out.println("salary:"+resultSet.getDouble("salary"));
+                System.out.println("department:"+resultSet.getString("department"));
+                System.out.println("Joining_Date:"+resultSet.getDate("Joining_Date"));
+                System.out.println("Gender:"+resultSet.getString("Gender"));
+            }
+        } catch (Exception e) {
+            //  e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
